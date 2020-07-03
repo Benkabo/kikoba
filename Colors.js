ec300b77c1bcbf157a1d36812cba53a46a1d43a5
Benedict <kaboyokabenedict@gmail.com>
2020-07-03 09:20:33 +0300

    Updated src/components

---
+++ Colors.js
@@ -0,0 +1,9 @@
+export default colors = {
+    blue: "#448DF1",
+    darkblue: "#04396C",
+    black: '#000000',
+    lightgreen: '#009387',
+    darkWhite: '#EAEDED',
+    lightblue: '#016AFB'
+  };
+  
