3f71cd592ced5cef813cf1afbfa9edc31ca5a973
Benedict <kaboyokabenedict@gmail.com>
2020-07-03 10:13:55 +0300

    Update App.js

--- App.js
+++ App.js
@@ -8,53 +8,12 @@ import Colors from "./Colors";
 import Jamii from "./src/components/Jamii";
 import RegisterScreen from "./src/screens/RegisterScreen";
 import Bima from "./src/components/Bima";
+import MainStackNavigator from "./src/navigation/MainStackNavigator";
 
 const Stack = createStackNavigator();
 
 export default function App() {
   return (
-    <NavigationContainer>
-      <Stack.Navigator
-        initialRouteName={StartScreen}
-        screenOptions={{
-          headerStyle: {
-            backgroundColor: Colors.lightblue,
-          },
-          headerTitleAlign: "center",
-          headerTintColor: Colors.darkWhite,
-        }}
-      >
-        <Stack.Screen
-          name="Loading"
-          component={StartScreen}
-          options={{ headerShown: false }}
-        />
-        <Stack.Screen
-          name="Login"
-          component={LoginScreen}
-          options={{ headerShown: false }}
-        />
-        <Stack.Screen
-          name="Signup"
-          component={RegisterScreen}
-          options={{ title: "REGISTER" }}
-        />
-        <Stack.Screen
-          name="Home"
-          component={HomePage}
-          options={{ title: "KARIBU" }}
-        />
-        <Stack.Screen
-          name="Jamii"
-          component={Jamii}
-          options={{ title: "MFUKO WA JAMII" }}
-        />
-        <Stack.Screen
-          name="Bima"
-          component={Bima}
-          options={{ title: "BIMA" }}
-        />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <MainStackNavigator />
   );
 }
