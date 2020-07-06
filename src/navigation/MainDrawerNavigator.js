1096079d5631345a4e3404a2f1bc68935c73753b
Benedict <kaboyokabenedict@gmail.com>
2020-07-06 17:23:34 +0300

    Update MainDrawerNavigator.js

--- src/navigation/MainDrawerNavigator.js
+++ src/navigation/MainDrawerNavigator.js
@@ -4,11 +4,12 @@ import { createStackNavigator } from "@react-navigation/stack";
 import AddAccount from "../components/AddAccount";
 import { NavigationContainer } from "@react-navigation/native";
 import HomePage from "../components/HomaPage";
+import { DrawerContent } from '../screens/DrawerContent' 
 
 const Drawer = createDrawerNavigator();
 const HomeStack = createStackNavigator();
 
-const HomeStackScreen = () => {
+HomeStackScreen = () => {
   <HomeStack.Navigator>
     <HomeStack.Screen
       name="Home"
@@ -21,8 +22,8 @@ const HomeStackScreen = () => {
 const MainDrawerNavigator = () => {
   return (
     <NavigationContainer>
-      <Drawer.Navigator>
-        <Drawer.Screen name="Home" children={HomeStackScreen()} />
+      <Drawer.Navigator drawerContent={props => <DrawerContent {...props} />}>
+        <Drawer.Screen name="Home" component={HomePage} />
       </Drawer.Navigator>
     </NavigationContainer>
   );
