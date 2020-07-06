495a3d9a7af70bd64976f1b9bbcf27c548fa2353
Benedict <kaboyokabenedict@gmail.com>
2020-07-06 12:08:31 +0300

    Update RegisterScreen.js

--- src/screens/RegisterScreen.js
+++ src/screens/RegisterScreen.js
@@ -89,7 +89,7 @@ export default function RegisterScreen() {
                 color: Colors.darkWhite,
               }}
             >
-              SAJIRI
+              SAJILI
             </Text>
           </TouchableOpacity>
         </View>
