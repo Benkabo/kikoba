41e4bba565fd786651b83b8445856e3df1081fee
Benedict <kaboyokabenedict@gmail.com>
2020-07-06 12:08:14 +0300

    Update App.js

--- App.js
+++ App.js
@@ -1,7 +1,21 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 import MainStackNavigator from "./src/navigation/MainStackNavigator";
+import Splash from "./src/screens/Splash";
+import MainDrawerNavigator from "./src/navigation/MainDrawerNavigator";
 
 export default function App() {
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setLoading(false);
+    }, 2000);
+  }, []);
+
+  if (loading) {
+    return <Splash />;
+  }
   return <MainStackNavigator />;
+  // return <MainDrawerNavigator />
 }
