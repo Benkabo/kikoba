30bed3f5328e23b17b29ad743093181b63391818
Benedict <kaboyokabenedict@gmail.com>
2020-07-08 18:24:27 +0300

    Update App.js

--- App.js
+++ App.js
@@ -1,4 +1,7 @@
 import React, { useState, useEffect } from "react";
+import { InMemoryCache } from 'apollo-cache-inmemory'
+import { ApolloClient } from 'apollo-client'
+import { HttpLink } from 'apollo-link-http'
 
 import MainStackNavigator from "./src/navigation/MainStackNavigator";
 import Splash from "./src/screens/Splash";
